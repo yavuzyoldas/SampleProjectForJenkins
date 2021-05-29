e30fda0394177d6f03215ef80b04809d38294026
yavuzyoldas <48646138+yavuzyoldas@users.noreply.github.com>
2021-05-29 17:42:20 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("----***Hello World & Welcome to Jenkins Traning***----");
+		System.out.println("----***Hello World & Welcome to Jenkins Traning  ***----");
+		System.out.println("----***Hello from GitHub  ***----");
 	}
 }
